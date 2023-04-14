b9cc3f719f462aadd73a8ba580a7ea6d25e05b0c
rb <r.brand@iwf.ch>
2023-04-14 08:08:28 +0200

    partial implementation of the "unpack" command

--- src/bin/elfshaker/unpack.rs
+++ src/bin/elfshaker/unpack.rs
@@ -1,32 +1,29 @@
 use std::error::Error;
 
 use clap::{App, Arg, ArgMatches};
-use log::{info, warn};
+use crate::utils::open_repo_from_cwd;
 
-use super::utils::{create_percentage_print_reporter, open_repo_from_cwd};
-use elfshaker::packidx::PackError;
-use elfshaker::repo::{Error as RepoError, ExtractOptions};
+use elfshaker::repo::{LOOSE_DIR, PackId, Repository, SnapshotId};
 
 pub(crate) const SUBCOMMAND: &str = "unpack";
 
 pub(crate) fn run(matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
-    let is_verify = matches.is_present("verify");
-    let is_force = matches.is_present("force");
+    // let is_verify = matches.is_present("verify");
+    // let is_force = matches.is_present("force");
 
     // Parse --threads
-    let threads: u32 = match matches.value_of("threads").unwrap().parse()? {
-        0 => {
-            let phys_cores = num_cpus::get_physical();
-            info!(
-                "-T|--threads=0: defaulting to number of physical cores (OS reports {} cores)",
-                phys_cores
-            );
-            phys_cores as u32
-        }
-        n => n,
-    };
+    // let threads: u32 = match matches.value_of("threads").unwrap().parse()? {
+    //     0 => {
+    //         let phys_cores = num_cpus::get_physical();
+    //         info!(
+    //             "-T|--threads=0: defaulting to number of physical cores (OS reports {} cores)",
+    //             phys_cores
+    //         );
+    //         phys_cores as u32
+    //     }
+    //     n => n,
+    // };
 
-    // let mut repo = open_repo_from_cwd()?;
     // let new_head = match repo.find_snapshot(snapshot) {
     //     Err(RepoError::PackError(PackError::SnapshotNotFound(_))) => {
     //         info!("Snapshot not available locally. Updating remotes...");
@@ -36,13 +33,21 @@ pub(crate) fn run(matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
     //     r => r?,
     // };
 
-    //TODO: complete implementation; just drafting out the idea
 
-    // clear LOOSE_DIR
 
-    // let pack_id = matches.value_of("pack").unwrap();
+    let mut repo = open_repo_from_cwd()?;
+
+    let pack_id = matches.value_of("pack").unwrap();
     // open pack
-    // let pack = repo.open_pack(pack_id.into()).unwrap();
+    let pack_id = PackId::Pack(pack_id.to_owned());
+    let pack = repo.open_pack(&pack_id).unwrap();
+
+    // TODO: get entries from index
+
+    // TODO: maybe clear LOOSE_DIR or handle overwrite
+
+    // let entries = None;
+    repo.extract_entries(&pack_id, &[], LOOSE_DIR, Default::default()).expect("could not extract");
 
     // iterate through snapshots and extract each of them to the LOOSE_DIR
     // pack.extract_entries()
@@ -82,6 +87,12 @@ pub(crate) fn run(matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
 pub(crate) fn get_app() -> App<'static, 'static> {
     App::new(SUBCOMMAND)
         .about("Can be used to unpack a pack to a loose pack.")
+        .arg(
+            Arg::with_name("pack")
+                .required(true)
+                .index(1)
+                .help("The tag of the pack to be unpacked."),
+        )
         .arg(
             Arg::with_name("verify")
                 .long("verify")
